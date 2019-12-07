819650a8360a778e901475ba789896366677ae08
Anatoliy Lipunov <anatoliy.lipunov@g5software.com>
2019-12-07 12:07:16 +0200

    Add gulp-autoprefixer package to gulpfile config

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,37 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
+var gulp = require ('gulp');
+var browserSync = require ('browser-sync').create ();
+var autoprefixer = require ('gulp-autoprefixer');
 
-gulp.task('browser-sync', function(done) {
-  browserSync.init({
+gulp.task ('browser-sync', function (done) {
+  browserSync.init ({
     server: {
       baseDir: '../gulp-config',
     },
     notify: false,
   });
 
-  browserSync.watch('../gulp-config').on('change', browserSync.reload);
+  browserSync.watch ('../gulp-config').on ('change', browserSync.reload);
 
-  done();
+  done ();
 });
 
-gulp.task('css', function(done) {
+gulp.task ('css', function (done) {
   gulp
-    .src('*.css')
-    .pipe(gulp.dest('styles'))
-    .pipe(browserSync.reload({stream: true}));
-
-  done();
+    .src ('*.css')
+    .pipe (gulp.dest ('styles'))
+    .pipe (
+      autoprefixer (['last 4 version', '> 5%', 'ie 11'], {
+        cascade: true,
+      })
+    )
+    .pipe (browserSync.reload ({stream: true}));
+  done ();
 });
 
-gulp.task(
+gulp.task (
   'watch',
-  gulp.series('css', 'browser-sync', function(done) {
-    gulp.watch('*.*', gulp.series('css'));
-    done();
+  gulp.series ('css', 'browser-sync', function (done) {
+    gulp.watch ('*.*', gulp.series ('css'));
+    done ();
   })
 );
